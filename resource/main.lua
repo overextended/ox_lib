55a857fbf80439ce0747ca6818807b37c3f75a6a
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-16 19:37:36 +1100

    chore: vscode is dumb
    all these spaces make a tab

--- resource/main.lua
+++ resource/main.lua
@@ -2,7 +2,8 @@
 -- Hacky workaround for Lua Language Server support.
 
 lib = setmetatable({}, {
-    __newindex = function(self, name, fn)
-        exports(name, fn)
-    end
+	__newindex = function(self, name, fn)
+		print('created '..name)
+		exports(name, fn)
+	end
 })
--- version.lua
+++ version.lua
@@ -1,28 +1,28 @@
 CreateThread(function()
-    Wait(1000)
-    local resource = GetCurrentResourceName()
-    local url = GetResourceMetadata(resource, 'repository', 0)
-    local version = GetResourceMetadata(resource, 'version', 0)
+	Wait(1000)
+	local resource = GetCurrentResourceName()
+	local url = GetResourceMetadata(resource, 'repository', 0)
+	local version = GetResourceMetadata(resource, 'version', 0)
 
-    PerformHttpRequest(('%s/master/fxmanifest.lua'):format(url:gsub('github.com', 'raw.githubusercontent.com')), function(error, response)
-        if error ~= 200 then
-            local latest = response:match('%d%.%d+%.%d+')
+	PerformHttpRequest(('%s/master/fxmanifest.lua'):format(url:gsub('github.com', 'raw.githubusercontent.com')), function(error, response)
+		if error ~= 200 then
+			local latest = response:match('%d%.%d+%.%d+')
 			print(version, latest)
-            if version == latest then
-                local curMajor, curMinor = string.strsplit('.', version)
-                local newMajor, newMinor =  string.strsplit('.', response:match('%d%.%d+%.%d+'))
-                local link = ('%s/archive/refs/tags/%s.zip'):format(url, latest, latest)
+			if version == latest then
+				local curMajor, curMinor = string.strsplit('.', version)
+				local newMajor, newMinor =  string.strsplit('.', response:match('%d%.%d+%.%d+'))
+				local link = ('%s/archive/refs/tags/%s.zip'):format(url, latest, latest)
 
-                if tonumber(curMajor) < tonumber(newMajor) then
-                    latest = 'A major update'
-                elseif tonumber(curMinor) < tonumber(newMinor) then
-                    latest = 'An update'
-                else
-                    latest = 'A patch'
-                end
+				if tonumber(curMajor) < tonumber(newMajor) then
+					latest = 'A major update'
+				elseif tonumber(curMinor) < tonumber(newMinor) then
+					latest = 'An update'
+				else
+					latest = 'A patch'
+				end
 
-                print(('^3%s is available for oxmysql - please download the latest release (current version: %s)\n   ^3- %s^0'):format(latest, version, link))
-            end
-        end
-    end, 'GET')
+				print(('^3%s is available for oxmysql - please download the latest release (current version: %s)\n   ^3- %s^0'):format(latest, version, link))
+			end
+		end
+	end, 'GET')
 end)
