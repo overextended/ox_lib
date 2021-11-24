9adbccd2f878ef6c69600c0f25443c0b196a128b
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-25 00:56:07 +1100

    feat(init): implement interval
    This is different from the one implemented in Ox ESX.

--- init.lua
+++ init.lua
@@ -35,6 +35,37 @@ function import(file)
 	return IMPORTS[file]
 end
 
+-----------------------------------------------------------------------------------------------
+-- Global functions
+-----------------------------------------------------------------------------------------------
+
+--- Dream of a world where this PR gets accepted.
+--- ```lua
+--- SetInterval(callback: function, timer: number)
+--- ```
+SetInterval = setmetatable({currentId = 0}, {
+	__call = function(self, callback, timer)
+		local id = self.currentId + 1
+		self.currentId = id
+		self[id] = timer or 0
+		CreateThread(function()
+			repeat
+				local interval = self[id]
+				Wait(interval)
+				callback(interval)
+			until interval == -1
+			self[id] = nil
+		end)
+		return id
+	end
+})
+
+function ClearInterval(id)
+	if SetInterval[id] then
+		SetInterval[id] = -1
+	end
+end
+
 -- pe-lualib
 -- Copyright (C) 2021	Linden <https://github.com/thelindat>
 
