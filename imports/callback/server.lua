4ae20d4dad8e6d9bef623202846e61d16213116b
Linden <65407488+thelindat@users.noreply.github.com>
2022-02-28 17:14:18 +1100

    refactor(imports/callback): new syntax
    BREAKING!

--- imports/callback/server.lua
+++ imports/callback/server.lua
@@ -1,14 +1,14 @@
-local ServerCallback = {}
+local callback = {}
 
 ---@param name string
 ---@param callback function
 --- Registers an event handler and callback function to respond to client requests.
-ServerCallback.Register = function(name, callback)
-	name = ('__cb_%s:%s'):format(GetCurrentResourceName(), name)
+function callback.register(name, cb)
+	name = ('__cb_%s'):format(name)
 
 	RegisterServerEvent(name, function(id, ...)
-		TriggerClientEvent(name..id, source, {callback(source, ...)})
+		TriggerClientEvent(name..id, source, {cb(source, ...)})
 	end)
 end
 
-return ServerCallback
+return callback
