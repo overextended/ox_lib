ff71a7d033be133b809cabef2934d0f9cdd63faa
Luke <sabolukas03@gmail.com>
2022-04-11 15:26:10 +0200

    feat(client): Input dialog function

---
+++ resource/interface/client/input.lua
@@ -0,0 +1,22 @@
+function lib.inputDialog(heading, rows)
+	if input then return end
+	input = promise.new()
+
+	SetNuiFocus(true, true)
+	SendNUIMessage({
+		action = 'openDialog',
+		data = {
+			heading = heading,
+			inputs = rows
+		}
+	})
+	return Citizen.Await(input)
+end
+
+RegisterNUICallback('inputData', function(data, cb)
+	cb(1)
+	if not input then return end
+	if not data then input:resolve() else input:resolve(data) end
+	SetNuiFocus(false, false)
+	input = nil
+end)
