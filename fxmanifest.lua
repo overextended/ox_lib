bc6a6c7a0a5fcfc254a18ad0a7fd229fbda23f06
Linden <65407488+thelindat@users.noreply.github.com>
2022-05-10 17:48:44 +1000

    fix: include locales in release workflow

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,7 +23,7 @@ game         'gta5'
 --[[ Resource Information ]]--
 name         'ox_lib'
 author       'Linden'
-version      '2.3.0'
+version      '2.3.1'
 repository   'https://github.com/overextended/ox_lib'
 description  'A library of shared functions to utilise in other resources.'
 
