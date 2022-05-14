14ce365de1cbc7201f2bdf3c4dd4fcbbac7a4273
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-05-14 12:34:12 +1000

    fix(zones): thickness workaround
    thickness is calculated incorrectly by glm.polygon.contains so this is required until it is fixed

--- imports/zones/client.lua
+++ imports/zones/client.lua
@@ -297,7 +297,7 @@ end
 local glm_polygon_contains = glm.polygon.contains
 
 local function contains(self, coords)
-	return glm_polygon_contains(self.polygon, coords, self.thickness)
+	return glm_polygon_contains(self.polygon, coords, self.thickness / 4)
 end
 
 local function insideSphere(self, coords)
