2599d2ed806001f3e01c630f07fd89a3464ef367
Linden <65407488+thelindat@users.noreply.github.com>
2022-05-29 10:33:34 +1000

    feat(getCore/server): initialise player module and add getter

--- imports/getCore/es_extended/server.lua
+++ imports/getCore/es_extended/server.lua
@@ -1,5 +1,20 @@
+if not lib.player then lib.player() end
+
 return function(resource)
 	local ESX = exports[resource]:getSharedObject()
 	-- Eventually add some functions here to simplify the creation of framework-agnostic resources.
+
+	local CPlayer = lib.getPlayer()
+
+	function lib.getPlayer(player)
+		player = type(player) == 'table' and player.playerId or ESX.GetPlayerFromId(player)
+
+		if not player then
+			error(("'%s' is not a valid player"):format(player))
+		end
+
+		return setmetatable(player, CPlayer)
+	end
+
 	return ESX
 end
--- imports/getCore/qb-core/server.lua
+++ imports/getCore/qb-core/server.lua
@@ -1,5 +1,20 @@
+if not lib.player then lib.player() end
+
 return function(resource)
 	local QBCore = exports[resource]:GetCoreObject()
 	-- Eventually add some functions here to simplify the creation of framework-agnostic resources.
+
+	local CPlayer = lib.getPlayer()
+
+	function lib.getPlayer(player)
+		player = type(player) == 'table' and player.playerId or QBCore.Functions.GetPlayer(player)
+
+		if not player then
+			error(("'%s' is not a valid player"):format(player))
+		end
+
+		return setmetatable(player, CPlayer)
+	end
+
 	return QBCore
 end
