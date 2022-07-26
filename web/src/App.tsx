0a2f5a9d4e0c51141dc81c000bc5c68d11d72dde
Luke <sabolukas03@gmail.com>
2022-07-26 15:35:22 +0200

    feat(web/menu): initial list menu design
    Highly subject to change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,12 +3,13 @@ import CircleProgressbar from "./features/progress/CircleProgressbar";
 import Progressbar from "./features/progress/Progressbar";
 import TextUI from "./features/textui/TextUI";
 import InputDialog from "./features/dialog/InputDialog";
-import ContextMenu from "./features/menu/ContextMenu";
+import ContextMenu from "./features/menu/context/ContextMenu";
 import Settings from "./features/settings";
 import { useNuiEvent } from "./hooks/useNuiEvent";
 import { setClipboard } from "./utils/setClipboard";
 import { fetchNui } from "./utils/fetchNui";
 import AlertDialog from "./features/dialog/AlertDialog";
+import ListMenu from "./features/menu/list";
 
 const App: React.FC = () => {
   useNuiEvent("setClipboard", (data: string) => {
@@ -27,6 +28,7 @@ const App: React.FC = () => {
       <InputDialog />
       <AlertDialog />
       <ContextMenu />
+      <ListMenu />
     </>
   );
 };
