98b9e224efe6b532528885229bab825688a47447
Luke <sabolukas03@gmail.com>
2022-07-27 20:22:45 +0200

    refactor(web/menu): list menu component split

---
+++ web/src/features/menu/list/Header.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Header: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <Box p={3} textAlign="center" borderTopLeftRadius="md" borderTopRightRadius="md" bg="#25262B">
+      <Text fontSize={24} textTransform="uppercase" fontWeight={300} fontFamily="Poppins">
+        {title}
+      </Text>
+    </Box>
+  );
+};
+
+export default Header;
---
+++ web/src/features/menu/list/ListItem.tsx
@@ -0,0 +1,50 @@
+import { Box, Flex, Stack, Spacer, Text } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { forwardRef } from "react";
+
+interface Item {
+  label: string;
+  value: string | string[];
+}
+
+interface Props {
+  item: Item;
+  index: number;
+}
+
+const ListItem = forwardRef<Array<HTMLDivElement | null>, Props>(({ item, index }, ref) => {
+  return (
+    <Box
+      bg="#25262B"
+      borderRadius="md"
+      tabIndex={index}
+      pointerEvents="none"
+      p={2}
+      height="60px"
+      key={`item-${index}`}
+      _focus={{ bg: "#373A40", outline: "none" }}
+      ref={(element) => {
+        if (ref)
+          // @ts-ignore i cba
+          return (ref.current = [...ref.current, element]);
+      }}
+    >
+      {Array.isArray(item.value) ? (
+        <Flex justifyContent="center" alignItems="center">
+          <Stack spacing={1}>
+            <Text color="#909296" textTransform="uppercase" fontSize={12} verticalAlign="middle">
+              {item.label}
+            </Text>
+            <Text>Nice cool</Text>
+          </Stack>
+          <Spacer />
+          <FontAwesomeIcon icon="arrows-left-right" fontSize={20} color="#909296" />
+        </Flex>
+      ) : (
+        item.label
+      )}
+    </Box>
+  );
+});
+
+export default ListItem;
