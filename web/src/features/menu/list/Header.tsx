9217aee2ee9821aa41e4e887503d98543b660d02
Luke <sabolukas03@gmail.com>
2022-07-29 18:35:21 +0200

    refactor(web/menu): separate header and body divs
    This allows for the header to be visible even when scrolling through the menu items

--- web/src/features/menu/list/Header.tsx
+++ web/src/features/menu/list/Header.tsx
@@ -2,7 +2,15 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Header: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <Box p={3} textAlign="center" borderTopLeftRadius="md" borderTopRightRadius="md" bg="#25262B">
+    <Box
+      p={3}
+      textAlign="center"
+      borderTopLeftRadius="md"
+      borderTopRightRadius="md"
+      bg="#25262B"
+      height="60px"
+      width="sm"
+    >
       <Text fontSize={24} textTransform="uppercase" fontWeight={300} fontFamily="Poppins">
         {title}
       </Text>
