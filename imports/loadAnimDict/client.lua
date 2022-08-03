a45584ac78b83ee7513cd9ee580805bce9ffc1c0
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-03 17:27:58 +1000

    Revert "feat(imports): loadAnimDict"
    This reverts commit 77fb9cf63794dd4a8a50f0f6c21a687d33970b23.

--- imports/loadAnimDict/client.lua
+++
@@ -1,9 +0,0 @@
----Loads an anim dictionary if it has not been loaded.
----Invalid anim dictionaries will never return.
----@param dict string
-return function(dict)
-	if not HasAnimDictLoaded(dict) then
-		RequestAnimDict(dict)
-		repeat Wait(0) until HasAnimDictLoaded(dict)
-	end
-end
