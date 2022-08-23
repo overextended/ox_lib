2391ffa8a7f50b20da4f2e6fc88fcf5348a21224
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2022-08-23 09:31:33 +0000

    chore: bump manifest version to v2.10.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game         'gta5'
 --[[ Resource Information ]]--
 name         'ox_lib'
 author       'Linden'
-version      '2.10.1'
+version      '2.10.2'
 license      'LGPL-3.0-or-later'
 repository   'https://github.com/overextended/ox_lib'
 description  'A library of shared functions to utilise in other resources.'
