5549fd4cff6c5f43eea41555559ee621dccec0cf
Luke <sabolukas03@gmail.com>
2022-09-17 20:21:06 +0200

    fix(package): use generics to define return type

--- package/shared/resource/cache/index.ts
+++ package/shared/resource/cache/index.ts
@@ -4,7 +4,7 @@ export const cache: Record<string, any> = new Proxy(
   },
   {
     get(target, key: string) {
-      let result = key ? target[key] : target;
+      const result = key ? target[key] : target;
       if (result) return result;
 
       AddEventHandler(`ox_lib:cache:${key}`, (value: any) => {
@@ -17,6 +17,6 @@ export const cache: Record<string, any> = new Proxy(
   }
 );
 
-export const onCache = (key: string, cb: (value: any) => void) => {
+export const onCache = <T = any>(key: string, cb: (value: T) => void) => {
   AddEventHandler(`ox_lib:cache:${key}`, cb);
 };
