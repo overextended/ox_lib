c218365f527bc5cf0e4bbbc02bb3c8031dc3a638
Waves966 <dmncdoucet@gmail.com>
2022-10-14 08:42:01 -0600

    refactor(keybinds): tweaks

---
+++ imports/addKeybind/client.lua
@@ -0,0 +1,48 @@
+---@class CKeybind
+---@field name string
+---@field description string
+---@field defaultKey? string
+---@field keybind string
+---@field disabled? boolean
+---@field disable? fun(self: CKeybind, toggle: boolean)
+---@field onPressed? fun(self: CKeybind)
+---@field onReleased? fun(self: CKeybind)
+
+local keybinds = {}
+
+local function disableKeybind(self, toggle)
+    keybinds[self.name].disabled = toggle
+end
+
+local IsPauseMenuActive = IsPauseMenuActive
+
+---@param data CKeybind
+---@return CKeybind
+function lib.addKeybind(data)
+    data.defaultKey = data.defaultKey or ''
+    data.keybind = GetControlInstructionalButton(0, joaat('+' .. data.name) | 0x80000000, true):sub(3)
+    data.disabled = data.disabled or false
+    data.disable = disableKeybind
+
+    RegisterCommand('+' .. data.name, function()
+        if not data.onPressed or data.disabled or IsPauseMenuActive() then return end
+        data:onPressed()
+    end)
+
+    RegisterCommand('-' .. data.name, function()
+        if not data.onReleased or data.disabled or IsPauseMenuActive() then return end
+        data:onReleased()
+    end)
+
+    RegisterKeyMapping('+' .. data.name, data.description, 'keyboard', data.keybind)
+
+    SetTimeout(500, function()
+        TriggerEvent('chat:removeSuggestion', ('/+%s'):format(data.name))
+        TriggerEvent('chat:removeSuggestion', ('/-%s'):format(data.name))
+    end)
+
+    keybinds[data.name] = data
+    return data
+end
+
+return lib.addKeybind
