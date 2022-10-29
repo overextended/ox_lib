c227282ce0968ad45560288d3b7335aab3c78b2a
Luke <sabolukas03@gmail.com>
2022-10-29 12:46:22 +0200

    refactor(web/menu): update checkbox styling

--- web/src/features/menu/list/CustomCheckbox.tsx
+++ web/src/features/menu/list/CustomCheckbox.tsx
@@ -4,33 +4,33 @@ const CustomCheckbox: React.FC<{ checked: boolean }> = ({ checked }) => {
   const { getCheckboxProps, getInputProps, htmlProps } = useCheckbox();
   return (
     <chakra.label
-      display='flex'
-      flexDirection='row'
-      alignItems='center'
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
       gridColumnGap={2}
-      pr={4}
-      cursor='pointer'
+      pr={3}
+      cursor="pointer"
       {...htmlProps}
     >
       <input {...getInputProps()} hidden />
       <Flex
-        alignItems='center'
-        justifyContent='center'
-        border='2px solid'
-        borderColor='#909296'
+        alignItems="center"
+        justifyContent="center"
+        border="2px solid"
+        borderColor="#909296"
         rounded={'sm'}
-        w={6}
-        h={6}
+        w={5}
+        h={5}
         {...getCheckboxProps()}
       >
         {checked && (
-          <Box w={5} h={5} bg='#25262B'>
-            <CheckboxIcon isChecked color={'#373A40'} />
+          <Box w={4} h={4} bg="#909296">
+            <CheckboxIcon isChecked color="#25262B" />
           </Box>
         )}
       </Flex>
     </chakra.label>
-  )
+  );
 };
 
 export default CustomCheckbox;
