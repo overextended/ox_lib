283ee42338e5473aaff22ec203eb2edbcb9470a3
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-11-10 15:43:45 +0100

    feat(resource/cache): previous value for onCache

--- resource/cache/client.lua
+++ resource/cache/client.lua
@@ -2,10 +2,10 @@ local cache = _ENV.cache
 cache.playerId = PlayerId()
 cache.serverId = GetPlayerServerId(cache.playerId)
 
-function cache:set(key, value)
+function cache:set(key, value, previousValue)
 	if value ~= self[key] then
 		self[key] = value
-		TriggerEvent(('ox_lib:cache:%s'):format(key), value)
+		TriggerEvent(('ox_lib:cache:%s'):format(key), value, previousValue)
 		return true
 	end
 end
@@ -18,29 +18,29 @@ local GetCurrentPedWeapon = GetCurrentPedWeapon
 CreateThread(function()
 	while true do
 		local ped = PlayerPedId()
-		cache:set('ped', ped)
+		cache:set('ped', ped, cache.ped)
 
 		local vehicle = GetVehiclePedIsIn(ped, false)
 
 		if vehicle > 0 then
-			cache:set('vehicle', vehicle)
+			cache:set('vehicle', vehicle, cache.vehicle)
 
 			if not cache.seat or GetPedInVehicleSeat(vehicle, cache.seat) ~= ped then
 				for i = -1, GetVehicleMaxNumberOfPassengers(vehicle) - 1 do
 					if GetPedInVehicleSeat(vehicle, i) == ped then
-						cache:set('seat', i)
+						cache:set('seat', i, cache.seat)
 						break
 					end
 				end
 			end
 		else
-			cache:set('vehicle', false)
-			cache:set('seat', false)
+			cache:set('vehicle', false, cache.vehicle)
+			cache:set('seat', false, cache.seat)
 		end
 
 		local hasWeapon, currentWeapon = GetCurrentPedWeapon(ped, true)
 
-		cache:set('weapon', hasWeapon and currentWeapon or false)
+		cache:set('weapon', hasWeapon and currentWeapon or false, cache.weapon)
 
 		Wait(100)
 	end
