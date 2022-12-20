overextended/ox_lib

commit 26538b8148a7f8c8df0e8bb82076143137e83f1e

refactor(imports): assign modules directly to lib namespace
Mostly a change for improved intellisense with sumneko lua.
Includes some type fixes and deprecation notices.

imports/raycast/client.lua
@@ -1,7 +1,6 @@
 local StartShapeTestLosProbe = StartShapeTestLosProbe
 local GetShapeTestResultIncludingMaterial = GetShapeTestResultIncludingMaterial
 local GetWorldCoordFromScreenCoord = GetWorldCoordFromScreenCoord
-local raycast = {}
 
 ---@param flags number? Defaults to 1|2|8|16 (see: https://docs.fivem.net/natives/?_0x377906D8A31E5586)
 ---@param p8 number? A bit mask with bits 1, 2, 4, or 7 relating to collider types. 4 and 7 are usually used.
@@ -10,7 +9,7 @@ local raycast = {}
 ---@return vector3 endCoords
 ---@return vector3 surfaceNormal
 ---@return number materialHash
-function raycast.cam(flags, p8)
+function lib.raycast.cam(flags, p8)
 	local coords, normal = GetWorldCoordFromScreenCoord(0.5, 0.5)
 	local destination = coords + normal * 10
 	local handle = StartShapeTestLosProbe(coords.x, coords.y, coords.z, destination.x, destination.y, destination.z,
@@ -27,4 +26,4 @@ function raycast.cam(flags, p8)
 	end
 end
 
-return raycast
+return lib.raycast

imports/callback/client.lua
@@ -67,7 +67,7 @@ lib.callback = setmetatable({}, {
 ---@param delay number prevent the event from being called for the given time
 --- Sends an event to the server and halts the current thread until a response is returned.
 function lib.callback.await(event, delay, ...)
-	return triggerServerCallback(_, event, delay, false, ...)
+	return triggerServerCallback(nil, event, delay, false, ...)
 end
 
 local function callbackResponse(success, result, ...)

imports/disableControls/client.lua
@@ -43,7 +43,7 @@ local keys = {}
 local DisableControlAction = DisableControlAction
 local pairs = pairs
 
-return setmetatable(disableControls, {
+lib.disableControls = setmetatable(disableControls, {
 	__index = keys,
 	__newindex = keys,
 	__call = function()
@@ -52,3 +52,5 @@ return setmetatable(disableControls, {
 		end
 	end
 })
+
+return lib.disableControls

imports/getClosestPlayer/client.lua
@@ -4,7 +4,7 @@
 ---@return number? playerId
 ---@return number? playerPed
 ---@return vector3? playerCoords
-return function(coords, maxDistance, includePlayer)
+function lib.getClosestPlayer(coords, maxDistance, includePlayer)
 	local players = GetActivePlayers()
 	local closestId, closestPed, closestCoords
 	maxDistance = maxDistance or 2.0
@@ -28,3 +28,5 @@ return function(coords, maxDistance, includePlayer)
 
 	return closestId, closestPed, closestCoords
 end
+
+return lib.getClosestPlayer

imports/getClosestVehicle/client.lua
@@ -3,7 +3,7 @@
 ---@param includePlayerVehicle boolean Whether or not to include the player's current vehicle.
 ---@return number? vehicle
 ---@return vector3? vehicleCoords
-return function(coords, maxDistance, includePlayerVehicle)
+function lib.getClosestVehicle(coords, maxDistance, includePlayerVehicle)
 	local vehicles = GetGamePool('CVehicle')
 	local closestVehicle, closestCoords
 	maxDistance = maxDistance or 2.0
@@ -25,3 +25,5 @@ return function(coords, maxDistance, includePlayerVehicle)
 
 	return closestVehicle, closestCoords
 end
+
+return lib.getClosestVehicle

imports/getNearbyPlayers/client.lua
@@ -2,30 +2,32 @@
 ---@param maxDistance number The max distance to check.
 ---@param includePlayer boolean Whether or not to include the current player.
 ---@return number[]
-return function(coords, maxDistance, includePlayer)
-	local players = GetActivePlayers()
-	local nearby = {}
-	local count = 0
-	maxDistance = maxDistance or 2.0
+function lib.getNearbyPlayers(coords, maxDistance, includePlayer)
+    local players = GetActivePlayers()
+    local nearby = {}
+    local count = 0
+    maxDistance = maxDistance or 2.0
 
-	for i = 1, #players do
-		local playerId = players[i]
+    for i = 1, #players do
+        local playerId = players[i]
 
-		if playerId ~= cache.playerId or includePlayer then
-			local playerPed = GetPlayerPed(playerId)
-			local playerCoords = GetEntityCoords(playerPed)
-			local distance = #(coords - playerCoords)
+        if playerId ~= cache.playerId or includePlayer then
+            local playerPed = GetPlayerPed(playerId)
+            local playerCoords = GetEntityCoords(playerPed)
+            local distance = #(coords - playerCoords)
 
-			if distance < maxDistance then
-				count += 1
-				nearby[count] = {
-					id = playerId,
-					ped = playerPed,
-					coords = playerCoords,
-				}
-			end
-		end
-	end
+            if distance < maxDistance then
+                count += 1
+                nearby[count] = {
+                    id = playerId,
+                    ped = playerPed,
+                    coords = playerCoords,
+                }
+            end
+        end
+    end
 
-	return nearby
+    return nearby
 end
+
+return lib.getNearbyPlayers

imports/getNearbyVehicles/client.lua
@@ -2,7 +2,7 @@
 ---@param maxDistance number The max distance to check.
 ---@param includePlayerVehicle boolean Whether or not to include the player's current vehicle.
 ---@return number[]
-return function(coords, maxDistance, includePlayerVehicle)
+function lib.getNearbyVehicles(coords, maxDistance, includePlayerVehicle)
 	local vehicles = GetGamePool('CVehicle')
 	local nearby = {}
 	local count = 0
@@ -27,3 +27,5 @@ return function(coords, maxDistance, includePlayerVehicle)
 
 	return nearby
 end
+
+return lib.getNearbyVehicles

imports/player/client.lua
@@ -1,3 +1,8 @@
+--[[
+	This module was experimental and won't be worked on or used further.
+	May be removed in the future.
+]]
+
 local CPlayer = {}
 
 function CPlayer:__index(index, ...)
@@ -22,6 +27,7 @@ function CPlayer:getDistance(coords)
 	return #(self:getCoords() - coords)
 end
 
+---@deprecated
 function lib.getPlayer()
 	return CPlayer
 end

commit b688b98bbad0897b4e255d762ad63a524029c2f6

fix(client/raycast): missing table
This was broken with 26538b8

imports/raycast/client.lua
@@ -1,3 +1,5 @@
+lib.raycast = {}
+
 local StartShapeTestLosProbe = StartShapeTestLosProbe
 local GetShapeTestResultIncludingMaterial = GetShapeTestResultIncludingMaterial
 local GetWorldCoordFromScreenCoord = GetWorldCoordFromScreenCoord