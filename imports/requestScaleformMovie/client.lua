65b1e8466a4fb258935eaa32b0db5549c0fec975
ANTOND <71350868+antond15@users.noreply.github.com>
2022-12-27 21:05:03 +0100

    fix(imports/scaleform): return scaleform handle

--- imports/requestScaleformMovie/client.lua
+++ imports/requestScaleformMovie/client.lua
@@ -1,22 +1,22 @@
 ---Load a scaleform movie. When called from a thread, it will yield until it has loaded.
 ---@param scaleformName string
 ---@param timeout number? Number of ticks to wait for the scaleform movie to load. Default is 500.
----@return string? scaleformName
+---@return number? scaleform
 function lib.requestScaleformMovie(scaleformName, timeout)
-    if HasScaleformMovieLoaded(scaleformName) then return scaleformName end
-
     if type(scaleformName) ~= 'string' then
         error(("expected scaleformName to have type 'string' (received %s)"):format(type(scaleformName)))
     end
 
-    RequestScaleformMovie(scaleformName)
+    local scaleform = RequestScaleformMovie(scaleformName)
+
+    if HasScaleformMovieLoaded(scaleform) then return scaleform end
 
     if coroutine.running() then
         timeout = tonumber(timeout) or 500
 
         for _ = 1, timeout do
-            if HasScaleformMovieLoaded(scaleformName) then
-                return scaleformName
+            if HasScaleformMovieLoaded(scaleform) then
+                return scaleform
             end
 
             Wait(0)
@@ -25,7 +25,7 @@ function lib.requestScaleformMovie(scaleformName, timeout)
         print(("failed to load scaleformName '%s' after %s ticks"):format(scaleformName, timeout))
     end
 
-    return scaleformName
+    return scaleform
 end
 
 return lib.requestScaleformMovie
