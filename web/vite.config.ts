2468613102d17e5275b421b42fbc44dd21aa6aa9
masonschafercodes <57614646+masonschafercodes@users.noreply.github.com>
2023-02-01 12:10:15 -0500

    chore(web): migrate to vite v4
    Updated vite to v4 and update the react-plugin to support the v4 changes. Made sure dev port was stays consistent and manually set it to :3000

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: "build",
     target: "esnext",
