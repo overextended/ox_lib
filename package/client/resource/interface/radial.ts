b99014eb8c93972fa1abe5ec321440835cc334b1
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-02-15 18:10:39 +0100

    feat(package): add radial menu exports

---
+++ package/client/resource/interface/radial.ts
@@ -0,0 +1,17 @@
+import { IconName, IconPrefix } from '@fortawesome/fontawesome-common-types';
+
+type RadialItem = {
+  id: string;
+  label: string;
+  icon: IconName | [IconPrefix, IconName];
+  onSelect?: () => void;
+  menu?: string;
+};
+
+export const addRadialItem = (items: RadialItem | RadialItem[]) => exports.ox_lib.addRadialItem(items);
+
+export const removeRadialItem = (item: string) => exports.ox_lib.removeRadialItem(item);
+
+export const registerRadial = (radial: { id: string; items: RadialItem[] }) => exports.ox_lib.registerRadial(radial);
+
+export const hideRadial = () => exports.ox_lib.hideRadial();
