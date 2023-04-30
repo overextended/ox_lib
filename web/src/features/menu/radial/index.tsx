e3a8f2c4bd13629e72568ae3c98b44a5e209e432
Jag <jag3dagster@gmail.com>
2023-04-30 16:49:00 -0700

    chore: update to `  \n` to match markdown

--- web/src/features/menu/radial/index.tsx
+++ web/src/features/menu/radial/index.tsx
@@ -155,9 +155,9 @@ const RadialMenu: React.FC = () => {
                         height={25}
                         fixedWidth
                       />
-                      <text x={iconX} y={iconY + (item.label.includes("\n") ? 7 : 25)} fill="#fff" textAnchor="middle" pointerEvents="none">
-                        {item.label.includes("\n")
-                          ? item.label.split("\n").map((value) => <tspan x={iconX} dy="1.2em">{value}</tspan>)
+                      <text x={iconX} y={iconY + (item.label.includes("  \n") ? 7 : 25)} fill="#fff" textAnchor="middle" pointerEvents="none">
+                        {item.label.includes("  \n")
+                          ? item.label.split("  \n").map((value) => <tspan x={iconX} dy="1.2em">{value}</tspan>)
                           : item.label
                         }
                       </text>
