bce3a0fd711cf2386276b4dd64049470605f1047
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-09-09 00:24:45 +0200

    feat(components/date): format date conversion

--- web/src/features/dialog/components/fields/date.tsx
+++ web/src/features/dialog/components/fields/date.tsx
@@ -1,13 +1,20 @@
 import { IDateInput } from '../../../../typings/dialog';
 import { Control, useController } from 'react-hook-form';
 import { FormValues } from '../../InputDialog';
-import { DatePicker, DateRangePicker, TimeInput } from '@mantine/dates';
+import { DatePicker, DateRangePicker } from '@mantine/dates';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface Props {
   row: IDateInput;
   index: number;
   control: Control<FormValues>;
+}
+
+const convertDateToString = (date: Date, format: string): string => {
+  format.replace('DD', date.getDate().toString());
+  format.replace('MM', date.getMonth().toString());
+  format.replace('YYYY', date.getFullYear().toString());
+  return format;
 }
 
 const DateField: React.FC<Props> = (props) => {
@@ -26,7 +33,7 @@ const DateField: React.FC<Props> = (props) => {
           ref={controller.field.ref}
           onBlur={controller.field.onBlur}
           // Workaround to use timestamp instead of Date object in values
-          onChange={(date) => controller.field.onChange(date ? date.getTime() : null)}
+          onChange={(date) => controller.field.onChange(date ? (props.row.useFormat && props.row.format ? convertDateToString(date, props.row.format) : date.getTime()) : null)}
           label={props.row.label}
           description={props.row.description}
           placeholder={props.row.format}
