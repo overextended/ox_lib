c9ba529f0b2726d0196068fc4ad78777f9fc01a4
Linden <65407488+thelindat@users.noreply.github.com>
2023-09-16 22:13:18 +1000

    refactor(require): module path resolution for imported files
    If loading an external file with fxmanifest (e.g. server_file '@src/file.lua'),
    require will load from @src rather than the current resource context.

--- imports/require/shared.lua
+++ imports/require/shared.lua
@@ -31,15 +31,35 @@ function lib.require(modname)
             return result
         end
 
+        local idx, resourceSrc = 1
+
+        while true do
+            local di = debug.getinfo(idx, 'S')
+
+            if not di or di.short_src:find('^@' .. cache.resource) then
+                resourceSrc = cache.resource
+                break
+            elseif di.short_src:find('^citizen') then
+                resourceSrc = idx == 2 and cache.resource or debug.getinfo(idx - 1, 'S').short_src:gsub('^@(.-)/.+', '%1')
+                break
+            end
+
+            idx += 1
+        end
+
         local modpath = modname:gsub('%.', '/')
 
         for path in package.path:gmatch('[^;]+') do
             local scriptPath = path:gsub('?', modpath):gsub('%.+%/+', '')
-            local resourceFile = LoadResourceFile(cache.resource, scriptPath)
+            local resourceFile = LoadResourceFile(resourceSrc, scriptPath)
+
+            if resourceSrc ~= cache.resource then
+                modname = ('@%s.%s'):format(resourceSrc, modname)
+            end
 
             if resourceFile then
                 loaded[modname] = false
-                scriptPath = ('@@%s/%s'):format(cache.resource, scriptPath)
+                scriptPath = ('@@%s/%s'):format(resourceSrc, scriptPath)
 
                 local chunk, err = load(resourceFile, scriptPath)
 
