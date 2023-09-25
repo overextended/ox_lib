6fdced51d7b1868b17e6d05b6686c2c0299e10f1
Manason <clanerp@gmail.com>
2023-09-25 08:30:42 -0700

    feat: added lib.registerNetEvent

---
+++ imports/registerNetEvent/shared.lua
@@ -0,0 +1,11 @@
+---Registers a net event that cannot be called from the same side of the network
+---@param event string
+---@param fn function
+function lib.registerNetEvent(event, fn)
+    RegisterNetEvent(event, function(...)
+        if GetInvokingResource() then return end
+        fn(...)
+    end)
+end
+
+return lib.registerNetEvent
