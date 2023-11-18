e4c4db04ab77b1ff0830f149803d338bef0daa94
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-11-18 16:20:38 +0100

    chore(web): update vite

---
+++ web/vite.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+    target: 'esnext',
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+});
