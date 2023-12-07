048edfa86f33fdcf4b12851c272ebc82d9f39b69
Linden <65407488+thelindat@users.noreply.github.com>
2023-12-07 18:24:14 +1100

    feat(require): add load/loadjson functions

--- imports/require/shared.lua
+++ imports/require/shared.lua
@@ -11,24 +11,10 @@ package = {
 
 local _require = require
 
----Loads the given module inside the current resource, returning any values returned by the file or `true` when `nil`.
----@param modname string
----@return unknown?
-function lib.require(modname)
-    if type(modname) ~= 'string' then return end
-
-    local modpath = modname:gsub('%.', '/')
-    local module = loaded[modname]
-
-    if module then return module end
-
-    local success, result = pcall(_require, modname)
-
-    if success then
-        loaded[modname] = result
-        return result
-    end
-
+---@param modpath string
+---@param modname? string
+---@return string, string?
+local function getModuleInfo(modpath, modname)
     local resourceSrc
 
     if not modpath:find('^@') then
@@ -50,9 +36,99 @@ function lib.require(modname)
             idx += 1
         end
 
-        if resourceSrc ~= cache.resource then
+        if modname and resourceSrc ~= cache.resource then
             modname = ('@%s.%s'):format(resourceSrc, modname)
         end
+    end
+
+    return resourceSrc, modname
+end
+
+---@param filePath string
+---@param env? table
+---@return any
+---Loads and runs a Lua file at the given path. Unlike require, the chunk is not cached for future use.
+function lib.load(filePath, env)
+    local resourceSrc
+    local modpath = filePath:gsub('%.', '/')
+
+    if not modpath:find('^@') then
+        resourceSrc = getModuleInfo(modpath)
+    end
+
+    if not resourceSrc then
+        resourceSrc = modpath:gsub('^@(.-)/.+', '%1')
+        modpath = modpath:sub(#resourceSrc + 3)
+    end
+
+    for path in package.path:gmatch('[^;]+') do
+        local scriptPath = path:gsub('?', modpath):gsub('%.+%/+', '')
+        local resourceFile = LoadResourceFile(resourceSrc, scriptPath)
+
+        if resourceFile then
+            filePath = ('@@%s/%s'):format(resourceSrc, filePath)
+
+            local chunk, err = load(resourceFile, filePath, 't', env or _ENV)
+
+            if not chunk or err then
+                error(err or 'an unknown error occurred', 2)
+            end
+
+            return chunk()
+        end
+    end
+
+    error(('cannot load file at path %s'):format(filePath))
+end
+
+---@param filePath string
+---@return table
+---Loads and decodes a json file at the given path.
+function lib.loadJson(filePath)
+    local resourceSrc
+    local modpath = filePath:gsub('%.', '/')
+
+    if not modpath:find('^@') then
+        resourceSrc = getModuleInfo(modpath)
+    end
+
+    if not resourceSrc then
+        resourceSrc = modpath:gsub('^@(.-)/.+', '%1')
+        modpath = modpath:sub(#resourceSrc + 3)
+    end
+
+    local scriptPath = ('%s.json'):format(modpath)
+    local resourceFile = LoadResourceFile(resourceSrc, scriptPath)
+
+    if resourceFile then
+        return json.decode(resourceFile)
+    end
+
+    error(('cannot load json file at path %s'):format(filePath))
+end
+
+---Loads the given module inside the current resource, returning any values returned by the file or `true` when `nil`.
+---@param modname string
+---@return unknown?
+function lib.require(modname)
+    if type(modname) ~= 'string' then return end
+
+    local modpath = modname:gsub('%.', '/')
+    local module = loaded[modname]
+
+    if module then return module end
+
+    local success, result = pcall(_require, modname)
+
+    if success then
+        loaded[modname] = result
+        return result
+    end
+
+    local resourceSrc
+
+    if not modpath:find('^@') then
+        resourceSrc, modname = getModuleInfo(modpath, modname) --[[@as string]]
     end
 
     if not module then
