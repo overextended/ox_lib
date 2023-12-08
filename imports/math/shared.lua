b8a5056a0ef217f01b0fb2fa98b338300ff7a9e2
ali-exacute <alinikbakht.98ia@gmail.com>
2023-12-08 12:30:12 +0330

    lowercase the function

--- imports/math/shared.lua
+++ imports/math/shared.lua
@@ -119,7 +119,7 @@ end
 ---@param lower number
 ---@param upper number
 ---@return number
-function math.Clamp(val, lower, upper) -- credit https://love2d.org/forums/viewtopic.php?t=1856
+function math.clamp(val, lower, upper) -- credit https://love2d.org/forums/viewtopic.php?t=1856
     if lower > upper then lower, upper = upper, lower end -- swap if boundaries supplied the wrong way
     return math.max(lower, math.min(upper, val))
 end
