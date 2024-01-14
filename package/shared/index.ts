2ad35039856222da06d09d74dec8e220ca5c0379
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-14 12:34:13 +1100

    feat(package/locale): update for parity with lua
    Support for nested locales (flattening) and getLocale function.

--- package/shared/index.ts
+++ package/shared/index.ts
@@ -1,1 +1,8 @@
 export * from './resource';
+
+// https://www.raygesualdo.com/posts/flattening-object-keys-with-typescript-types/
+export type FlattenObjectKeys<T extends Record<string, unknown>, Key = keyof T> = Key extends string
+  ? T[Key] extends Record<string, unknown>
+    ? `${Key}.${FlattenObjectKeys<T[Key]>}`
+    : `${Key}`
+  : never;
