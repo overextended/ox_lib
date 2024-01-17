c0832cbe72c781471cadcd9c84f5597335a4d45a
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-17 17:39:16 +1100

    feat(package): add waitFor and sleep functions

--- package/shared/index.ts
+++ package/shared/index.ts
@@ -6,3 +6,35 @@ export type FlattenObjectKeys<T extends Record<string, unknown>, Key = keyof T> 
     ? `${Key}.${FlattenObjectKeys<T[Key]>}`
     : `${Key}`
   : never;
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms, null));
+}
+
+export async function waitFor<T>(cb: () => T, errMessage: string | void, timeout = 1000): Promise<T> {
+  let value = await cb();
+
+  if (value !== undefined) return value;
+
+  if (IsDuplicityVersion()) timeout /= 50;
+  else timeout -= GetFrameTime() * 1000;
+
+  const start = GetGameTimer();
+  let id: number;
+  let i = 0;
+
+  const p = new Promise<T>((resolve, reject) => {
+    id = setTick(async () => {
+      i++;
+
+      if (i > timeout)
+        return reject(`${errMessage || 'failed to resolve callback'} (waited ${(GetGameTimer() - start) / 1000}ms)`);
+
+      value = await cb();
+
+      if (value !== undefined) resolve(value);
+    });
+  }).finally(() => clearTick(id));
+
+  return p;
+}
