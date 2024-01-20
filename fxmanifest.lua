d4d7ee86b1451232cecce0cf24486f4197427f4a
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 06:36:30 +0000

    chore: bump manifest version to v3.16.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 --[[ Resource Information ]]--
 name         'ox_lib'
 author       'Overextended'
-version      '3.16.1'
+version      '3.16.2'
 license      'LGPL-3.0-or-later'
 repository   'https://github.com/overextended/ox_lib'
 description  'A library of shared functions to utilise in other resources.'
