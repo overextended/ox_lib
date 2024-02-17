4e8614b0edd136e575453cee515d9ab5a91130cc
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-02-17 12:10:05 +0100

    fix(interface/input): clearable lua type (#498)

--- resource/interface/client/input.lua
+++ resource/interface/client/input.lua
@@ -18,7 +18,7 @@ local input
 ---@field required? boolean
 ---@field format? string
 ---@field returnString? boolean
----@field clearable? string
+---@field clearable? boolean
 ---@field description? string
 
 ---@class InputDialogOptionsProps
