d8578281eeb1e0968cf8910c3a3128a2f8396ed4
xSamiVS <samtaiebc@gmail.com>
2024-03-09 11:04:18 +0100

    feat(web/textui) bottom center position(#507)

--- package/client/resource/interface/textui.ts
+++ package/client/resource/interface/textui.ts
@@ -4,7 +4,7 @@ import { CSSProperties } from 'react';
 type IconAnimation = 'spin' | 'spinPulse' | 'spinReverse' | 'pulse' | 'beat' | 'fade' | 'beatFade' | 'bounce' | 'shake';
 
 interface OptionsProps {
-  position?: 'right-center' | 'left-center' | 'top-center';
+  position?: 'right-center' | 'left-center' | 'top-center' | 'bottom-center';
   icon?: IconName | [IconPrefix, IconName];
   iconColor?: string;
   iconAnimation?: IconAnimation;
