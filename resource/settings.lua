77c34b869934434be191c3457bd59d86bedd4eca
Linden <65407488+thelindat@users.noreply.github.com>
2024-04-03 14:58:35 +1100

    feat(settings): toggle notification audio

--- resource/settings.lua
+++ resource/settings.lua
@@ -1,5 +1,6 @@
 local settings = {
     locale = GetResourceKvpString('locale'),
+    notification_audio = GetResourceKvpInt('notification_audio') == 1
 }
 
 RegisterCommand('ox_lib', function()
@@ -11,12 +12,22 @@ RegisterCommand('ox_lib', function()
             description = locale('ui.settings.locale_description', settings.locale),
             options = GlobalState['ox_lib:locales'],
             icon = 'book',
+        },
+        {
+            type = 'checkbox',
+            label = locale('ui.settings.notification_audio'),
+            checked = settings.notification_audio,
         }
     }) --[[@as any]]
 
     if not input then return end
 
     if input[1] then lib.setLocale(input[1]) end
+
+    if input[2] ~= nil then
+        settings.notification_audio = input[2]
+        SetResourceKvpInt('notification_audio', input[2] and 1 or 0)
+    end
 end)
 
 return settings
