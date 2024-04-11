761b52a03b8a758295e86416f250bcce9577c1d1
ANTOND <71350868+antond15@users.noreply.github.com>
2024-04-11 03:22:41 +0200

    fix(requestScaleformMovie): use scaleform handle (#543)

--- imports/requestScaleformMovie/client.lua
+++ imports/requestScaleformMovie/client.lua
@@ -1,13 +1,17 @@
 ---Load a scaleform movie. When called from a thread, it will yield until it has loaded.
 ---@param scaleformName string
 ---@param timeout number? Approximate milliseconds to wait for the scaleform movie to load. Default is 1000.
----@return number scaleform
+---@return number? scaleform
 function lib.requestScaleformMovie(scaleformName, timeout)
     if type(scaleformName) ~= 'string' then
         error(("expected scaleformName to have type 'string' (received %s)"):format(type(scaleformName)))
     end
 
-    return lib.streamingRequest(RequestScaleformMovie, HasScaleformMovieLoaded, 'scaleformMovie', scaleformName, timeout)
+    local scaleform = RequestScaleformMovie(scaleformName)
+
+    return lib.waitFor(function()
+        if HasScaleformMovieLoaded(scaleform) then return scaleform end
+    end, ("failed to load scaleformMovie '%s'"):format(scaleformName), timeout)
 end
 
 return lib.requestScaleformMovie
