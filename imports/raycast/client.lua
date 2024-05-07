a3deb29ae477302b1b2c6c59e1082b7ac564acf9
Linden <65407488+thelindat@users.noreply.github.com>
2024-05-08 04:25:51 +1000

    refactor(raycast): minor perf and native tweaks

--- imports/raycast/client.lua
+++ imports/raycast/client.lua
@@ -2,10 +2,15 @@ lib.raycast = {}
 
 local StartShapeTestLosProbe = StartShapeTestLosProbe
 local GetShapeTestResultIncludingMaterial = GetShapeTestResultIncludingMaterial
+local glm_sincos = require 'glm'.sincos
+local glm_rad = require 'glm'.rad
+local math_abs = math.abs
+local GetFinalRenderedCamCoord = GetFinalRenderedCamCoord
+local GetFinalRenderedCamRot = GetFinalRenderedCamRot
 
-local function GetForwardVector(rotation)
-    local rot = (math.pi / 180.0) * rotation
-    return vector3(-math.sin(rot.z) * math.abs(math.cos(rot.x)), math.cos(rot.z) * math.abs(math.cos(rot.x)), math.sin(rot.x))
+local function getForwardVector()
+    local sin, cos = glm_sincos(glm_rad(GetFinalRenderedCamRot(2)))
+    return vec3(-sin.z * math_abs(cos.x), cos.z * math_abs(cos.x), sin.x)
 end
 
 ---@alias ShapetestIgnore
@@ -24,21 +29,19 @@ end
 ---@return vector3 surfaceNormal
 ---@return number materialHash
 function lib.raycast.cam(flags, ignore, distance)
-	local coords = GetGameplayCamCoord()
-    local forwardVectors = GetForwardVector(GetGameplayCamRot(2))
-    local destination = coords + (forwardVectors * (distance or 10.0))
-	local handle = StartShapeTestLosProbe(coords.x, coords.y, coords.z, destination.x, destination.y, destination.z,
-		flags or 511, cache.ped, ignore or 4)
+    local camCoords = GetFinalRenderedCamCoord()
+    local destination = camCoords + getForwardVector() * (distance or 10)
+    local handle = StartShapeTestLosProbe(camCoords.x, camCoords.y, camCoords.z, destination.x, destination.y,
+        destination.z, flags or 511, cache.ped, ignore or 4)
 
-	while true do
-		Wait(0)
-		local retval, hit, endCoords, surfaceNormal, materialHash, entityHit = GetShapeTestResultIncludingMaterial(handle)
+    while true do
+        Wait(0)
+        local retval, hit, coords, surfaceNormal, materialHash, entityHit = GetShapeTestResultIncludingMaterial(handle)
 
-		if retval ~= 1 then
-            ---@diagnostic disable-next-line: return-type-mismatch
-			return hit, entityHit, endCoords, surfaceNormal, materialHash
-		end
-	end
+        if retval ~= 1 then
+            return hit, entityHit, coords, surfaceNormal, materialHash
+        end
+    end
 end
 
 return lib.raycast
