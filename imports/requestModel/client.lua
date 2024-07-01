e12a7fd27382666d17bf324ae9f337ed89911c76
Solareon <769465+solareon@users.noreply.github.com>
2024-07-01 13:18:51 +0200

    fix(client/requestModel): model validation and streaming request (#610)

--- imports/requestModel/client.lua
+++ imports/requestModel/client.lua
@@ -6,11 +6,11 @@ function lib.requestModel(model, timeout)
     if type(model) ~= 'number' then model = joaat(model) end
     if HasModelLoaded(model) then return model end
 
-    if not IsModelValid(model) then
+    if not IsModelValid(model) and not IsModelInCdimage(model) then
         error(("attempted to load invalid model '%s'"):format(model))
     end
 
-    return lib.streamingRequest(IsModelInCdimage, HasModelLoaded, 'model', model, timeout)
+    return lib.streamingRequest(RequestModel, HasModelLoaded, 'model', model, timeout)
 end
 
 return lib.requestModel
