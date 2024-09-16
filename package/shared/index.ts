e1c1647343f53048e3541a022f99eb712250131d
Linden <65407488+thelindat@users.noreply.github.com>
2024-09-16 18:34:55 +1000

    fix(package): specify side-effects and do not bundle output

--- package/shared/index.ts
+++ package/shared/index.ts
@@ -1,7 +1,7 @@
 export * from './resource';
 
 // https://www.raygesualdo.com/posts/flattening-object-keys-with-typescript-types/
-export type FlattenObjectKeys<T extends Record<string, unknown>, Key = keyof T> = Key extends string
+export type FlattenObjectKeys<T extends Record<string, any>, Key = keyof T> = Key extends string
   ? T[Key] extends Record<string, unknown>
     ? `${Key}.${FlattenObjectKeys<T[Key]>}`
     : `${Key}`
