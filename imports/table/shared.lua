ff4f79319e358c5f145ad4b5ff3d0b5dd2bc2bc7
Guru Saran <98257031+BigSmoKe07@users.noreply.github.com>
2024-10-01 06:58:23 +0000

    refactor: Optimize table_merge for performance

--- imports/table/shared.lua
+++ imports/table/shared.lua
@@ -81,17 +81,18 @@ end
 ---@return table
 ---Merges two tables together. Defaults to adding duplicate keys together if they are numbers, otherwise they are overriden.
 local function table_merge(t1, t2, addDuplicateNumbers)
-    if addDuplicateNumbers == nil then addDuplicateNumbers = true end
-    for k, v in pairs(t2) do
-        local type1 = type(t1[k])
-        local type2 = type(v)
+    addDuplicateNumbers = addDuplicateNumbers ~= nil and addDuplicateNumbers or true
+    for k, v2 in pairs(t2) do
+        local v1 = t1[k]
+        local type1 = type(v1)
+        local type2 = type(v2)
 
 		if type1 == 'table' and type2 == 'table' then
-            table_merge(t1[k], v, addDuplicateNumbers)
+            table_merge(v1, v2, addDuplicateNumbers)
         elseif addDuplicateNumbers and (type1 == 'number' and type2 == 'number') then
-            t1[k] += v
+            t1[k] = v1 + v2
 		else
-			t1[k] = v
+			t1[k] = v2
         end
     end
 
