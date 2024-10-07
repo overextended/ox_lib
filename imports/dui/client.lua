458810697a93dd862bd7dc0fb965049b11ca5e32
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-10-07 21:39:41 +0200

    fix(client/dui): use const instead let and debug sent data in message

--- imports/dui/client.lua
+++ imports/dui/client.lua
@@ -23,66 +23,66 @@ local currentId = 0
 
 ---@param data DuiProperties
 function lib.dui:constructor(data)
-	local time = GetGameTimer()
-	local id = ("%s_%s_%s"):format(cache.resource, time, currentId)
-	currentId = currentId + 1
-	local dictName = ('ox_lib_dui_dict_%s'):format(id)
-	local txtName = ('ox_lib_dui_txt_%s'):format(id)
-	local duiObject = CreateDui(data.url, data.width, data.height)
-	local duiHandle = GetDuiHandle(duiObject)
-	local runtimeTxd = CreateRuntimeTxd(dictName)
-	local txdObject = CreateRuntimeTextureFromDuiHandle(runtimeTxd, txtName, duiHandle)
-	self.id = id
-	self.debug = data.debug or false
-	self.url = data.url
-	self.duiObject = duiObject
-	self.duiHandle = duiHandle
-	self.runtimeTxd = runtimeTxd
-	self.txdObject = txdObject
-	self.dictName = dictName
-	self.txtName = txtName
-	duis[id] = self
+    local time = GetGameTimer()
+    local id = ("%s_%s_%s"):format(cache.resource, time, currentId)
+    currentId = currentId + 1
+    local dictName = ('ox_lib_dui_dict_%s'):format(id)
+    local txtName = ('ox_lib_dui_txt_%s'):format(id)
+    local duiObject = CreateDui(data.url, data.width, data.height)
+    local duiHandle = GetDuiHandle(duiObject)
+    local runtimeTxd = CreateRuntimeTxd(dictName)
+    local txdObject = CreateRuntimeTextureFromDuiHandle(runtimeTxd, txtName, duiHandle)
+    self.id = id
+    self.debug = data.debug or false
+    self.url = data.url
+    self.duiObject = duiObject
+    self.duiHandle = duiHandle
+    self.runtimeTxd = runtimeTxd
+    self.txdObject = txdObject
+    self.dictName = dictName
+    self.txtName = txtName
+    duis[id] = self
 
-	if self.debug then
-		print(('Dui %s created'):format(id))
-	end
+    if self.debug then
+        print(('Dui %s created'):format(id))
+    end
 end
 
 function lib.dui:remove()
-	SetDuiUrl(self.duiObject, 'about:blank')
-	DestroyDui(self.duiObject)
-	duis[self.id] = nil
+    SetDuiUrl(self.duiObject, 'about:blank')
+    DestroyDui(self.duiObject)
+    duis[self.id] = nil
 
-	if self.debug then
-		print(('Dui %s removed'):format(self.id))
-	end
+    if self.debug then
+        print(('Dui %s removed'):format(self.id))
+    end
 end
 
 ---@param url string
 function lib.dui:setUrl(url)
-	self.url = url
-	SetDuiUrl(self.duiObject, url)
+    self.url = url
+    SetDuiUrl(self.duiObject, url)
 
-	if self.debug then
-		print(('Dui %s url set to %s'):format(self.id, url))
-	end
+    if self.debug then
+        print(('Dui %s url set to %s'):format(self.id, url))
+    end
 end
 
 ---@param message table
 function lib.dui:sendMessage(message)
-	SendDuiMessage(self.duiObject, json.encode(message))
+    SendDuiMessage(self.duiObject, json.encode(message))
 
-	if self.debug then
-		print(('Dui %s message sent'):format(self.id))
-	end
+    if self.debug then
+        print(('Dui %s message sent with data :'):format(self.id), json.encode(message, { indent = true }))
+    end
 end
 
 AddEventHandler('onResourceStop', function(resourceName)
-	if cache.resource ~= resourceName then return end
+    if cache.resource ~= resourceName then return end
 
-	for _, dui in pairs(duis) do
-		dui:remove()
-	end
+    for _, dui in pairs(duis) do
+        dui:remove()
+    end
 end)
 
 return lib.dui
