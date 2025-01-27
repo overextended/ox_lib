71087d9b26312c12f96401ce554b867e4fc1aa04
hakanesnn <106948628+hakanesnn@users.noreply.github.com>
2025-01-27 16:26:13 +0300

    fix(cron): matching weekdayMap with os.date

--- imports/cron/server.lua
+++ imports/cron/server.lua
@@ -51,13 +51,13 @@ local maxUnits = {
 }
 
 local weekdayMap = {
-    sun = 0,
-    mon = 1,
-    tue = 2,
-    wed = 3,
-    thu = 4,
-    fri = 5,
-    sat = 6,
+    sun = 1,
+    mon = 2,
+    tue = 3,
+    wed = 4,
+    thu = 5,
+    fri = 6,
+    sat = 7,
 }
 
 local monthMap = {
