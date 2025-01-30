7518be69f629e1e6dde96362c72636817040799b
Linden <65407488+thelindat@users.noreply.github.com>
2025-01-31 10:22:50 +1100

    refactor(getRelativeCoords): overload for vector4 support
    Return a vec3/vec4 based on the type of coords, and allow
    heading to use either coords.w or heading (if defined).
    Minor performance improvements.

--- imports/getRelativeCoords/shared.lua
+++ imports/getRelativeCoords/shared.lua
@@ -1,20 +1,30 @@
---Get the relative coordinates of a vector3 based on a heading and offset
----@param coords vector3
+local glm_sincos = require 'glm'.sincos
+local glm_rad = require 'glm'.rad
+
+---Get the relative coordinates of a vector3 based on a heading and offset
+---@overload fun(coords: vector3, heading: number, offset: vector3): vector3
+---@overload fun(coords: vector4, offset: vector3): vector4
+---@param coords vector3 | vector4
 ---@param heading number
 ---@param offset vector3
----@return vector4 coords
+---@return vector3
 function lib.getRelativeCoords(coords, heading, offset)
-	local headingRad = math.rad(heading)
+    offset = offset or heading
+    local x, y, z, w = coords.x, coords.y, coords.z, type(heading) == 'number' and heading or coords.w
+    local sin, cos = glm_sincos --[[@as fun(n: number): number, number]](glm_rad(w))
+    local relativeX = offset.x * cos - offset.y * sin
+    local relativeY = offset.x * sin + offset.y * cos
 
-    local relativeX = offset.x * math.cos(headingRad) - offset.y * math.sin(headingRad)
-    local relativeY = offset.x * math.sin(headingRad) + offset.y * math.cos(headingRad)
-
-    return vec4(
-		coords.x + relativeX,
-		coords.y + relativeY,
-        coords.z + offset.z,
-        heading
-	)
+    return coords.w and vec4(
+        x + relativeX,
+        y + relativeY,
+        z + offset.z,
+        w
+    ) or vec3(
+        x + relativeX,
+        y + relativeY,
+        z + offset.z
+    )
 end
 
 return lib.getRelativeCoords
