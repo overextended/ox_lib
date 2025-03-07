b12a96e00068375a2e2e19d359820ff6358b5f5b
tom-osborne <tom.j.osborne@gmail.com>
2025-03-07 20:46:13 +0000

    fix(web): Remove unused fonts and switch to Inter. Fixes #721.

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -2,7 +2,7 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'dark',
-  fontFamily: 'Roboto',
+  fontFamily: 'Inter',
   shadows: { sm: '1px 1px 3px rgba(0, 0, 0, 0.5)' },
   components: {
     Button: {
