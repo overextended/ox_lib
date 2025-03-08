e01e728d1585adea288af4b37fc3e973994c2cbc
tom-osborne <tom.j.osborne@gmail.com>
2025-03-08 15:58:55 +0000

    chore(web): Switch to self-hosted Roboto font

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -2,7 +2,7 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'dark',
-  fontFamily: 'Inter',
+  fontFamily: 'Roboto',
   shadows: { sm: '1px 1px 3px rgba(0, 0, 0, 0.5)' },
   components: {
     Button: {
