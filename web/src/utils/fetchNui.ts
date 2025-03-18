f0df7ab696e255a0617f35acc64d2fedb55fe4f9
Linden <65407488+thelindat@users.noreply.github.com>
2025-03-18 16:55:04 +1100

    refactor(web/fetchNui): fetch stub
    Just a naive way to patch some common exploits.

--- web/src/utils/fetchNui.ts
+++ web/src/utils/fetchNui.ts
@@ -9,6 +9,12 @@
  * @return returnData - A promise for the data sent back by the NuiCallbacks CB argument
  */
 
+const fetch = window.fetch;
+// @ts-expect-error
+window.fetch = () => {};
+// @ts-expect-error
+window.XMLHttpRequest = window.fetch;
+
 export async function fetchNui<T = any>(eventName: string, data?: any): Promise<T> {
   const options = {
     method: 'post',
@@ -23,7 +29,6 @@ export async function fetchNui<T = any>(eventName: string, data?: any): Promise<
     : 'nui-frame-app';
 
   const resp = await fetch(`https://${resourceName}/${eventName}`, options);
-
   const respFormatted = await resp.json();
 
   return respFormatted;
