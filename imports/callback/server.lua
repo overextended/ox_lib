bdaafd5605bee64aa148ff95ebefdf15737610fd
Kenshin13 <63159154+Kenshiin13@users.noreply.github.com>
2026-05-03 14:02:56 +0200

    fix(callback): scope register's event to local instead of leaking to _G

--- imports/callback/server.lua
+++ imports/callback/server.lua
@@ -114,7 +114,7 @@ local pcall = pcall
 ---Registers an event handler and callback function to respond to client requests.
 ---@diagnostic disable-next-line: duplicate-set-field
 function lib.callback.register(name, cb)
-    event = cbEvent:format(name)
+    local event = cbEvent:format(name)
 
     lib.setValidCallback(name, true)
 
