3295470099efd3a3c5b3daba1e4a3185cbe82d36
Linden <65407488+thelindat@users.noreply.github.com>
2026-05-21 11:25:05 +1000

    refactor(init): remove GetGamePool function
    A native was added some time ago.

--- init.lua
+++ init.lua
@@ -260,20 +260,6 @@ else
         TriggerClientEvent(notifyEvent, playerId, data)
     end
 
-    local poolNatives = {
-        CPed = GetAllPeds,
-        CObject = GetAllObjects,
-        CVehicle = GetAllVehicles,
-    }
-
-    ---@param poolName 'CPed' | 'CObject' | 'CVehicle'
-    ---@return number[]
-    ---Server-side parity for the `GetGamePool` client native.
-    function GetGamePool(poolName)
-        local fn = poolNatives[poolName]
-        return fn and fn() --[[@as number[] ]]
-    end
-
     ---@return number[]
     ---Server-side parity for the `GetPlayers` client native.
     function GetActivePlayers()
